f58d04e85d39f1fa4b0ed1fc3160b53d632ded22
Tim Emmert <tim.emmert@tum.de>
2023-03-04 15:20:36 +0100

    Enable computation of derivatives for rough bergomi models

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 # WOndering what is happening here? https://github.com/ryanmccrickerd/rough_bergomi/blob/master/notebooks/rbergomi.ipynb
 import jax
 import numpy as np
+from jax import grad, value_and_grad
 from matplotlib import pyplot as plt
 
-from util import cov
-from rbergomi import dW1, dW2, Y, dB, V, S
+import jax.numpy as jnp
+from rbergomi import simulate
 
 T = 1.0
 n = 100
 m = 30000
-S0=1
+S0 = 1.
 
 a = -0.43
 xi = 0.235 ** 2
@@ -18,25 +19,18 @@ eta = 1.9
 
 dt = 1.0 / n
 s = int(n * T)  # Steps
-t = np.linspace(0, T, 1 + s)[np.newaxis, :]  # Time grid
+t = jnp.linspace(0, T, 1 + s)[np.newaxis, :]  # Time grid
 
 # Construct hybrid scheme correlation structure for kappa = 1
-e = np.array([0, 0])
-c = cov(a, n)
 
+K = 1.3
 
-# np.random.seed(0)
+val_grad_sim = value_and_grad(simulate, argnums=(5, 6, 7, 8, 9, 10))
 
-# TODO: Remove asarray
-dW1 = np.asarray(dW1(e=e, c=c, m=m, s=s, rng_key=jax.random.PRNGKey(123)))
-dW2 = np.asarray(dW2(m=m, s=s, dt=dt, rng_key=jax.random.PRNGKey(123123)))
-
-Y = Y(m=m, s=s, a=a, n=n, dW=dW1)
-dB = dB(dW1=dW1, dW2=dW2, rho=rho)
-V = V(a=a, t=t, Y=Y, xi=xi, eta=eta)
-S = S(dt=dt, V=V, dB=dB, S0=S0)
-
-paths_plot = 10
+for i in range(10000):
+    S, gradient = val_grad_sim(m, s, n, dt, t, K, S0, a, rho, eta, xi)
+    print(i)
+"""paths_plot = 10
 plot, axes = plt.subplots()
 axes.plot(t[0, :], np.mean(S, axis=0), 'r')
 axes.plot(t[0, :], np.ones_like(t[0, :]), 'g')
@@ -46,4 +40,4 @@ if paths_plot > 0:
 axes.set_xlabel(r'$t$', fontsize=16)
 axes.set_ylabel(r'$S_t$', fontsize=16)
 plt.grid(True)
-plt.show()
+plt.show()"""
--- rbergomi.py
+++ rbergomi.py
@@ -1,102 +1,91 @@
+from functools import partial
+
 import jax
 import numpy as np
+from jax import jit
 
-from util import b, g
+from util import b, cov, g
+import jax.numpy as jnp
 
 
-def dW1(e, c, m, s, rng_key):
+def compute_dW1(e, c, m, s, rng_key):
     """
     Produces random numbers for variance process with required
     covariance structure.
     """
-    # np.random.multivariate_normal(e, c, (m, s))
     return jax.random.multivariate_normal(key=rng_key, mean=e, cov=c, shape=(m, s))
 
 
-def dW2(m, s, dt, rng_key):
+def compute_dW2(m, s, dt, rng_key):
     """
     Obtain orthogonal increments.
     """
-    # return np.random.randn(m, s) * np.sqrt(dt)
     return jax.random.normal(key=rng_key, shape=(m, s)) * np.sqrt(dt)
 
 
-def Y(m, s, a, n, dW):
+def compute_Y(m, s, a, n, dW):
     """
     Constructs Volterra process from appropriately
     correlated 2d Brownian increments.
     """
-    Y1 = np.zeros((m, 1 + s))  # Exact integrals
-    Y2 = np.zeros((m, 1 + s))  # Riemann sums
-
-    """
-    for i in np.arange(1, 1 + s, 1):
-        Y1[:, i] = dW[:, i - 1, 1]  # Assumes kappa = 1
-    """
-
 
     # Construct Y1 through exact integral
-    Y1 = np.concatenate((
-        np.zeros((m, 1)),
+    Y1 = jnp.concatenate((
+        jnp.zeros((m, 1)),
         dW[:, :, 1]
-    ), axis=1)
+    ), axis=1)  # Exact integrals
 
-    # Construct arrays for convolution
-    G = np.zeros(1 + s)  # Gamma
-    for k in np.arange(2, 1 + s, 1):
-        G[k] = g(b(k, a) / n, a)
+    ks = jnp.arange(2, 1 + s, 1)
+    G = jnp.concatenate((np.array([0, 0]), g(b(ks, a) / n, a)))
 
     X = dW[:, :, 0]  # Xi
 
-    # Initialise convolution result, GX
-    GX = np.zeros((m, len(X[0, :]) + len(G) - 1))
-
     # Compute convolution, FFT not used for small n
     # Possible to compute for all paths in C-layer?
-    for i in range(m):
-        GX[i, :] = np.convolve(G, X[i, :])
+    GX = jnp.apply_along_axis(func1d=lambda x: jnp.convolve(G, x), arr=X, axis=1)
 
     # Extract appropriate part of convolution
-    Y2 = GX[:, :1 + s]
+    Y2 = GX[:, :1 + s]  # Riemann sums
 
     # Finally contruct and return full process
-    Y = np.sqrt(2 * a + 1) * (Y1 + Y2)
+    Y = jnp.sqrt(2 * a + 1) * (Y1 + Y2)
     return Y
 
 
-
-
-def dB(dW1, dW2, rho=0.0):
+def compute_dB(dW1, dW2, rho=0.0):
     """
     Constructs correlated price Brownian increments, dB.
     """
-    dB = rho * dW1[:, :, 0] + np.sqrt(1 - rho ** 2) * dW2
+    dB = rho * dW1[:, :, 0] + jnp.sqrt(1 - rho ** 2) * dW2
     return dB
 
 
-def V(a, t, Y, xi=1.0, eta=1.0):
+def compute_V(a, t, Y, xi=1.0, eta=1.0):
     """
     rBergomi variance process.
     """
-    V = xi * np.exp(eta * Y - 0.5 * eta ** 2 * t ** (2 * a + 1))
+    V = xi * jnp.exp(eta * Y - 0.5 * eta ** 2 * t ** (2 * a + 1))
     return V
 
 
-def S(dt, V, dB, S0=1):
+def compute_S(m, dt, V, dB, S0=1):
     """
     rBergomi price process.
     """
 
     # Construct non-anticipative Riemann increments
-    increments = np.sqrt(V[:, :-1]) * dB - 0.5 * V[:, :-1] * dt
+    increments = jnp.sqrt(V[:, :-1]) * dB - 0.5 * V[:, :-1] * dt
 
     # Cumsum is a little slower than Python loop.
-    integral = np.cumsum(increments, axis=1)
+    integral = jnp.cumsum(increments, axis=1)
 
-    S = np.zeros_like(V)
-    S[:, 0] = S0
-    S[:, 1:] = S0 * np.exp(integral)
-    return S
+    return S0 * jnp.concatenate(
+        (
+            jnp.ones((m, 1,)),
+            jnp.exp(integral)
+        ),
+        axis=1
+    )
 
 
 def S1(dt, V, dW1, rho, S0=1):
@@ -113,3 +102,21 @@ def S1(dt, V, dW1, rho, S0=1):
     S[:, 0] = S0
     S[:, 1:] = S0 * np.exp(integral)
     return S
+
+
+@partial(jit, static_argnums=(0, 1, 2, 3))
+def simulate(m, s, n, dt, t, K, S0, a, rho, eta, xi):
+    e = jnp.array([0, 0])
+    c = cov(a, n)
+
+    dW1 = compute_dW1(e=e, c=c, m=m, s=s, rng_key=jax.random.PRNGKey(123))
+    dW2 = compute_dW2(m=m, s=s, dt=dt, rng_key=jax.random.PRNGKey(123123))
+
+    Y = compute_Y(m=m, s=s, a=a, n=n, dW=dW1)
+    dB = compute_dB(dW1=dW1, dW2=dW2, rho=rho)
+    V = compute_V(a=a, t=t, Y=Y, xi=xi, eta=eta)
+    S = compute_S(m=m, dt=dt, V=V, dB=dB, S0=S0)
+
+    res = (S[:, -1] > K) * S[:, -1]
+
+    return 1/m * res.sum()
